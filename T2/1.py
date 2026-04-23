28e07137af9458cc7f7451cd2cc72fa2e97f5b18
KrishMiyani <220610287+KrishMiyani13@users.noreply.github.com>
2026-04-23 10:45:27 +0530

    Refactor code structure for improved readability and maintainability

---
+++ T2/1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+import seaborn as sns
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix,accuracy_score,precision_score, recall_score
